6702ed637782df77d70b22141f9141eac9405c96
alxxw <alxxwu@gmail.com>
2022-07-08 18:54:03 +1000

    changed frontend

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -21,7 +21,7 @@ const InputTodo = () => {
   return (
     <Fragment>
       <h1 className="text-center mt-5"> message board </h1>;
-      <div>
+      <div className="mt-3">
         all opinions expressed do not reflect the creator of the website.
       </div>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
